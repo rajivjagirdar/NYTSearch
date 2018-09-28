06a2fc66ddd2e76e28e47d5783b7b16601582310
Kirby <legendofkirby85@gmail.com>
2018-09-28 00:19:48 -0700

    second commit

--- assets/javascript/dates.js
+++ assets/javascript/dates.js
@@ -17,3 +17,17 @@ var EndDate = 0;
 
 StartDate = $('#startdate').val().trim();
 EndDate = $('#enddate').val().trim();
+
+
+      // Constructing HTML containing articles with the search information requested 
+      var headline = $("<h1>").text(response.docs.headline);
+      var articleURL = $("<a>").attr("href", response.docs.web_url).append(//"search information");
+      var snippet = $("<h2>").text(response.docs.snippet);
+      var published = $("<h2>").text(response.docs.pub_date);
+   
+      // Empty the contents of the search-div, append the new articles
+      $("#search-div").empty();
+      $("#search-div").append(headline, published, snippet, articleURL);
+
+
+//probably need to change this and add in a loop to get all the articles on the page 
